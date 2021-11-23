ff5dda6ba62f3e3d1ae385e3cc7138bbe0596fc8
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2021-11-23 14:33:02 +0000

    groupBy concept finished

--- src/ops/group-by.ts
+++ src/ops/group-by.ts
@@ -1,18 +1,73 @@
 import {Piper} from '../types';
 
 /**
- * Check out: https://www.learnrxjs.io/learn-rxjs/operators/transformation/groupby
+ * Groups objects/values into arrays of those, based on the returned selector value.
  */
-export function groupBy<T, K>(keySelector: (value: T, index: number) => K): Piper<T, T[]> {
+export function groupBy<T>(keySelector: (value: T, index: number) => any): Piper<T, T[]>;
+
+/**
+ * Groups objects by property name as the object-key.
+ *
+ * This is usually followed by a "reduce" operator, to produce a single object:
+ * reduce((c, i) => ({...c, ...i}), {})
+ */
+export function groupBy<T>(property: keyof T): Piper<T, { property: T[] }>;
+
+/**
+ * Check out:
+ * - https://www.learnrxjs.io/learn-rxjs/operators/transformation/groupby
+ * - https://sebhastian.com/javascript-group-by
+ */
+export function groupBy<T>(selector: any): Piper<T, any> {
     return (iterable: Iterable<T>) => ({
         [Symbol.iterator](): Iterator<T[]> {
-            // const i = iterable[Symbol.iterator]();
+            const i = iterable[Symbol.iterator]();
             // let index = 0;
             return {
                 next(): IteratorResult<T[]> {
+                    const a = i.next();
+                    if (!a.done) {
+
+                    }
                     return {value: undefined, done: true};
                 }
             };
         }
     });
 }
+
+
+/*
+
+// examples:
+
+// this is useful, easy to remap into 1 object:
+const a1 = [
+    {1: ['one', 'two']},
+    {2: ['three', 'four']},
+    {3: ['111', '222']}
+];
+
+// this one is useless:
+const a2: [number, any[]][] = [
+    [1, ['one', 'two']],
+    [2, ['three', 'four']],
+    [3, ['111', '222']]
+];
+
+// this is the default:
+const a3 = [
+    [{key: 1, value: 'one'}, {key: 1, value: 'two'}],
+    [{key: 2, value: 'three'}, {key: 2, value: 'four'}],
+    [{key: 3, value: 'last'}]
+];
+
+const b1 = a1.reduce((c, i) => ({...c, ...i}), {});
+
+const b2 = a2.reduce((c, i) => ({...c, [i[0]]: i[1]}), {});
+
+// no can do, also pointless
+// const b3 = a3.reduce((c, i) => ({...c, [i[0]]: i[1]}), {});
+
+console.log(b2);
+*/
--- test/group-by.spec.ts
+++ test/group-by.spec.ts
@@ -2,18 +2,32 @@ import {expect} from './header';
 import {pipe, groupBy} from '../src';
 
 describe('groupBy', () => {
+    const people = [
+        {name: 'Sue', age: 25},
+        {name: 'Joe', age: 30},
+        {name: 'Frank', age: 25},
+        {name: 'Sarah', age: 35}
+    ];
     it('must group by selector', () => {
-        const people = [
-            {name: 'Sue', age: 25},
-            {name: 'Joe', age: 30},
-            {name: 'Frank', age: 25},
-            {name: 'Sarah', age: 35}
-        ];
         const output = pipe(people, groupBy(person => person.age));
         expect([...output]).to.eql([
             [{age: 25, name: 'Sue'}, {age: 25, name: 'Frank'}],
             [{age: 30, name: 'Joe'}],
             [{age: 35, name: 'Sarah'}]
+        ]);
+    });
+    it('must group by property', () => {
+        const output = pipe(people, groupBy('age'));
+        expect([...output]).to.eql([
+            {
+                25: [{age: 25, name: 'Sue'}, {age: 25, name: 'Frank'}]
+            },
+            {
+                30: [{age: 30, name: 'Joe'}]
+            },
+            {
+                35: [{age: 35, name: 'Sarah'}]
+            }
         ]);
     });
 });
